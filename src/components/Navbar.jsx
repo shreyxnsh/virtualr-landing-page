7cf804bc6f68b2c03d76b4e69b442ecce55eb21c
shreyxnsh <shreyanshatwork@gmail.com>
2024-11-23 23:11:47 +0530

    project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+// import React from 'react'
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { navItems } from "../constants";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+            <div className="container px-4 mx-auto relative text-sm">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center flex-shrink-0">
+                        <img src={logo} alt="logo" className="w-10 h-10 mr-2" />
+                        <span className="tracking-tight text-2xl">Virtualr</span>
+                    </div>
+
+                    <ul className="hidden lg:flex ml-14 space-x-12">
+                        {navItems.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="hidden lg:flex justify-center space-x-12 items-center">
+                        <a href="#" className="py-2 px-3 border rounded-md">
+                            Login
+                        </a>
+                        <a
+                            href="#"
+                            className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+                        >
+                            Create an account
+                        </a>
+                    </div>
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleNavbar}>
+                            {isOpen ? <X size={24} /> : <Menu size={24} />}
+                        </button>
+                    </div>
+                </div>
+                {isOpen && (
+                    <div className="lg:hidden fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center">
+
+                        <ul>
+                            {navItems.map((item, index) => (
+                                <li key={index} className="py-4 ">
+                                    <a href={item.href}>{item.label}</a>
+                                </li>
+                            ))}
+                            
+                        </ul>
+                        <div className="flex justify-center space-x-12 items-center">
+                            <a href="#" className="py-2 px-3 border rounded-md">
+                                Login
+                            </a>
+                            <a
+                                href="#"
+                                className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+                            >
+                                Create an account
+                            </a>
+                            </div>
+                                    
+                    </div>
+
+
+                )}
+            </div>
+        </nav>
+    );
+};
+
+
+export default Navbar;
