874b29be3d0a38117274c30c0ba76b4eec7eda08
shreyxnsh <shreyanshatwork@gmail.com>
2024-11-24 13:00:05 +0530

    footer + app.jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import Workflow from './components/WorkflowSection'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -8,7 +13,13 @@ const App = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6 ">
         <HeroSection />
+        <FeatureSection />
+      <Workflow />
+      <Pricing />
+      <Testimonials />
+      <Footer />
       </div>
+      
 
     </>
   )
